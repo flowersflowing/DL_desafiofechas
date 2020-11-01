e60d5d96b7c1e85b39e84abd33fc1c87332c2a3e
Natalia <nflores.teoria@gmail.com>
2020-11-01 18:55:51 -0300

    definiendo lógica y flujo

--- src/components/Cronometro.vue
+++ src/components/Cronometro.vue
@@ -2,6 +2,9 @@
   <div class="crono">
     <h3>Cuenta regresiva</h3>
     <div>
+      <!-- Agregar un div que muestre la cuenta regresiva con v-show o if -->
+      <div></div>
+      <!-- Debo agragar un click a mis botones que llamen al método -->
       <button class="btn">3s</button>
       <button class="btn">1m</button>
       <button class="btn">5m</button>
@@ -14,6 +17,19 @@
 <script>
 export default {
   name: 'Cronometro',
+  data() {
+    return {
+      timers: [
+      //Agregar los arrays
+      ]
+    }
+  },
+  // Agregar método para la cuenta regresiva. Almacenar y pasar la variable. Agregar booliano para que se activo el v-show
+  // methods: {
+  //   setUpTimer(){
+
+  //   }    
+  // },
 }
 </script>
 
