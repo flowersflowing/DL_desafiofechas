221a4cb09ab661997c7de25d4cf96aca99baed4e
Natalia <nflores.teoria@gmail.com>
2020-11-01 14:11:04 -0300

    primeras adecuaciones, inserción de botones

--- src/components/Cronometro.vue
+++ src/components/Cronometro.vue
@@ -1,17 +1,25 @@
 <template>
   <div class="crono">
+    <h3>Cuenta regresiva</h3>
+    <div>
+      <button class="btn">3s</button>
+      <button class="btn">1m</button>
+      <button class="btn">5m</button>
+      <button class="btn">10m</button>
+      <button class="btn">30m</button>      
+    </div>
   </div>
 </template>
 
 <script>
 export default {
   name: 'Cronometro',
-  props: {
-    // msg: String
-  }
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+.btn {
+  margin: 4px;
+}
 </style>
