9c8a2955a293ec48e1c735c07f3e05488683a34d
Natalia <nflores.teoria@gmail.com>
2020-10-31 22:52:40 -0300

    iniciando proyecto

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Cronometro/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Cronometro from './components/Cronometro.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Cronometro
   }
 }
 </script>
