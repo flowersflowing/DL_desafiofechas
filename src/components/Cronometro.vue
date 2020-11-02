75c2af0a1c0667538116db28b2329fb751a3bf23
Natalia <nflores.teoria@gmail.com>
2020-11-02 11:59:56 -0300

    aún casi pura definición de flujo, agrego array en data

--- src/components/Cronometro.vue
+++ src/components/Cronometro.vue
@@ -2,14 +2,14 @@
   <div class="crono">
     <h3>Cuenta regresiva</h3>
     <div>
-      <!-- Agregar un div que muestre la cuenta regresiva con v-show o if -->
+      <!-- Agregar un div que muestre la cuenta regresiva con v-show o if. Debe mostrar {{timer.tiempo}}.-->
       <div></div>
-      <!-- Debo agragar un click a mis botones que llamen al método -->
-      <button class="btn">3s</button>
-      <button class="btn">1m</button>
-      <button class="btn">5m</button>
-      <button class="btn">10m</button>
-      <button class="btn">30m</button>      
+      <!-- Debo agregar un click a mis botones que llamen al método -->
+      <button class="btn" @click="setUpTimer(3000)">3s</button>
+      <button class="btn" @click="setUpTimer(60000)">1m</button>
+      <button class="btn" @click="setUpTimer(300000)">5m</button>
+      <button class="btn" @click="setUpTimer(600000)">10m</button>
+      <button class="btn" @click="setUpTimer(1800000)">30m</button>      
     </div>
   </div>
 </template>
@@ -21,15 +21,20 @@ export default {
     return {
       timers: [
       //Agregar los arrays
+        {nombre: '3', tiempo: '00:03'},
+        {nombre: '1', tiempo: '01:00'},
+        {nombre: '5', tiempo: '05:00'},
+        {nombre: '10', tiempo: '10:00'},
+        {nombre: '30', tiempo: '30:00'},
       ]
     }
   },
-  // Agregar método para la cuenta regresiva. Almacenar y pasar la variable. Agregar booliano para que se activo el v-show
-  // methods: {
-  //   setUpTimer(){
-
-  //   }    
-  // },
+  // Agregar método para la cuenta regresiva. Almacenar y pasar la variable. Agregar booliano para que se active el v-show
+  methods: {
+    setUpTimer(ms){
+      console.log(ms); //entrando
+    }    
+  },
 }
 </script>
 
